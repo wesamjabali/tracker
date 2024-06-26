e089d94363b4314e6524790ab648e4eba7dcf39f
Wesam Jabali <wesam.jabali@aldi-sued.com>
2024-06-26 15:55:34 +0200

    stop zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,18 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt"],
-  app: { head: { title: "Easy tracker" } },
+  app: {
+    head: {
+      title: "Easy tracker",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
 });
