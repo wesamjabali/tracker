20dff5f68bd62da42dc9c23250bbe10db2d4c9e1
Wesam Jabali <wesamjabali@icloud.com>
2024-06-30 01:37:06 +0200

    add subkeys

--- app.vue
+++ app.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="layout">
+
     <div class="section" v-for="key of keys" :key="key">
       <label :for="`input-${key}`">{{ key }}:</label>
-      <input
-        @keydown.enter="submit"
-        :id="`input-${key}`"
-        v-model="currentResponse[key]"
-      />
+      <input @keydown.enter="submit" :id="`input-${key}`" v-if="!currentResponse[key]?.subKeys?.length"
+        v-model="currentResponse[key].value" />
+      <div v-for="subKey, index in currentResponse[key]?.subKeys">
+        <label :for="`input-${key}-${subKey.key}`">{{ subKey.key }}:</label>
+        <input :key="index" v-model.trim="subKey.value" @keydown.enter="submit" />
+      </div>
       <button style="margin: 0.5rem" @click="deleteKey(key)">Delete</button>
     </div>
+
+
     <div class="section" style="border: 1px solid green">
       <label for="new">New key:</label>
       <input @keydown.enter="createNewKey" id="newKey" v-model.trim="newKey" />
+      <span class="labeled-checkbox">
+        <label for="subkeys">Subkeys:</label>
+        <input @input="({ target: { checked } }) => checked ? subKeys = [''] : subKeys = []" id="subkeys"
+          type="checkbox" v-model="isSubKeysVisible" />
+      </span>
+      <span v-show="isSubKeysVisible && newKey">
+        <input v-for="subKey, index in subKeys" :key="index" v-model.trim="subKeys[index]"
+          @input="!subKeys.filter(v => v === '')?.length && subKeys.push('')" @keydown.enter="createNewKey" />
+      </span>
     </div>
-    <button
-      style="margin: auto; margin-bottom: 1rem; margin-top: 1rem; width: 100%"
-      @click="submit"
-    >
+
+
+    <button style="margin: auto; margin-bottom: 1rem; margin-top: 1rem; width: 100%" @click="submit">
       Submit
     </button>
     <div style="padding-left: 0.5rem">
       <div>
         Deleted keys:
-        <button
-          v-for="key of deletedKeys"
-          @click="deletedKeys = deletedKeys.filter((k) => k !== key)"
-        >
+        <button v-for="key of deletedKeys" @click="deletedKeys = deletedKeys.filter((k) => k !== key)">
           {{ key }}
         </button>
       </div>
@@ -33,38 +42,31 @@
         Responses: (click on table header to see graph)
         <table>
           <tr>
-            <th
-              @click="selectedKey = key"
-              v-for="key of [' ', 'date', ...keys]"
-              :key="key"
-            >
+            <th @click="selectedKey = key" v-for="key of [' ', 'date', ...keys]" :key="key">
               {{ key }}
             </th>
           </tr>
           <tr v-for="response of responses" :key="response.date">
             <td v-for="key of ['delete', 'date', ...keys]" :key="key">
-              <button
-                v-if="key === 'delete'"
-                style="font-size: 0.5rem;  margin: 0; padding: 0.25rem"
-                @click="responses = responses.filter((r) => r !== response)"
-              >
+              <button v-if="key === 'delete'" style="font-size: 0.5rem;  margin: 0; padding: 0.25rem"
+                @click="responses = responses.filter((r) => r !== response)">
                 Delete
               </button>
               <span v-else>
-                {{ response[key] }}
+                <span v-if="response[key]?.value"> {{ response[key]?.value }} </span>
+                <span v-for="r in response[key]?.subKeys">
+                  {{ r.value && `${r.key}: ${r.value} ` }}
+                </span>
               </span>
             </td>
           </tr>
         </table>
       </div>
       <div>
-        <button
-          class="bad-button"
-          @click="
-            responses = [];
-            deletedKeys = [];
-          "
-        >
+        <button class="bad-button" @click="
+      responses = [];
+    deletedKeys = [];
+    ">
           Clear all
         </button>
         <apexchart v-show="selectedKey" :options="options" :series="series" />
@@ -74,6 +76,7 @@
 </template>
 
 <script setup lang="ts">
+// @ts-nocheck
 let options = ref({
   chart: {
     id: "line-graph",
@@ -98,11 +101,22 @@ const getTemplateResponse = () => {
     if (ignoreKeys.value.includes(key)) {
       continue;
     }
-    templateResponse[key] = "";
+
+    const fullKeyResponse = [...responses.value.flatMap((response) => response[key])][0];
+
+    templateResponse[key] = { key, value: '', subKeys: fullKeyResponse.subKeys?.flatMap((subKey: any) => ({ key: subKey.key, value: '' })) }
+
+
+    templateResponse[key]?.subKeys?.length ?
+      delete templateResponse[key].value :
+      delete templateResponse[key]?.subKeys;
   }
+
   return templateResponse;
 };
 
+const subKeys = ref([]);
+const isSubKeysVisible = ref(false);
 const newKey = ref("");
 const responses = useLocalStorage<Array<any>>("responses", []);
 const deletedKeys = useLocalStorage<Array<string>>("deletedKeys", []);
@@ -118,11 +132,15 @@ const submit = () => {
     Object.entries(currentResponse.value).filter(([_key, value]) => value)
   );
 
-  if (Object.keys(strippedResponse).length === 0) {
+  if (Object.keys(strippedResponse)?.length === 0) {
     return;
   }
 
   strippedResponse.date = new Date().toLocaleString();
+  strippedResponse?.subKeys?.length ?
+    delete strippedResponse.value :
+    delete strippedResponse.subKeys;
+
   responses.value.unshift(strippedResponse);
   currentResponse.value = getTemplateResponse();
 };
@@ -133,38 +151,74 @@ const deleteKey = (key: string) => {
 
 const createNewKey = () => {
   if (newKey.value) {
-    const strippedKey = newKey.value.toLowerCase().trim().replace(/\s+/g, " ");
+    const strip = (v: string) => v.toLowerCase().trim().replace(/\s+/g, " ");
+    const strippedKey = strip(newKey.value);
+    const strippedSubKeys = subKeys.value.filter((v) => v).map(strip).map(key => ({ key, value: '' }));
 
-    currentResponse.value[strippedKey] = "";
+
+    currentResponse.value[strippedKey] = strippedSubKeys.length ? { key: strippedKey, subKeys: strippedSubKeys } : { key: strippedKey, subKeys: strippedSubKeys };
     deletedKeys.value = deletedKeys.value.filter((key) => key !== newKey.value);
     newKey.value = "";
+    subKeys.value = [];
   }
 };
 
-watch([selectedKey, () => responses.value.length], () => {
-  if (!selectedKey.value) {
-    return;
-  }
-
-  const sortedResponses = responses.value
-    .filter((r) => !!r[selectedKey.value] && parseInt(r[selectedKey.value]))
-    .toReversed();
-
-  if (!sortedResponses.length) {
+watch([selectedKey, () => responses.value?.length], () => {
+  if (!selectedKey.value || selectedKey.value === 'date') {
     selectedKey.value = "";
     return;
   }
 
-  options.value = {
-    chart: {id: 'line-graph'},
-    xaxis: { categories : sortedResponses.map((response) => response.date) },
+  const rawValues = responses.value.flatMap((response) => ({ ...response[selectedKey.value], date: response.date }));
+
+  const removeEmpty = (v) => {
+    if (!v || !Array.isArray(v)) return []
+    return v.filter((v) => !!v.value && parseInt(v.value))
+  }
+
+  const subKeyValues = removeEmpty(rawValues?.flatMap((v) => [...(v?.subKeys || [])?.map((subKey) => ({ ...subKey || {}, date: v.date }))]))
+  const singleValues = removeEmpty(rawValues?.filter((v) => !v.subKeys?.length))
+
+  const values = subKeyValues.length ? subKeyValues.toReversed() : singleValues.toReversed();
+  if (!values?.length) {
+    selectedKey.value = "";
+    return
   };
 
-  const newSeries = sortedResponses.map(
-    (response) => response[selectedKey.value]
+
+
+  options.value = {
+    chart: { id: 'line-graph' },
+    xaxis: { categories: values.map((value) => value.date) },
+  };
+
+  series.value = []
+  console.log('values', values)
+  const chunks = {}
+  for (const value of values) {
+    if (!chunks[value.key]) {
+      chunks[value.key] = []
+    }
+    chunks[value.key].push(value.value || -1)
+  }
+
+  series.value = Object.entries(chunks).map(([key, value]) => ({ name: key, data: value }))
+
+  console.log('chunks', chunks)
+
+  const newSeries = values.map(
+    (value) => value.value
   );
 
-  series.value = [{ name: selectedKey.value, data: newSeries }];
+
+  console.log('new', newSeries)
+
+
+
+
+
+
+
 });
 </script>
 
@@ -185,6 +239,7 @@ watch([selectedKey, () => responses.value.length], () => {
 .section {
   display: flex;
 }
+
 .section label {
   width: 6rem;
   text-transform: capitalize;
@@ -236,5 +291,21 @@ table tr:nth-child(even) {
 
 table tr:hover {
   background-color: #ddd;
+}
+
+.labeled-checkbox {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  margin: 0.5rem;
+}
+
+.labeled-checkbox label {
+  margin: 0;
+  padding: 0;
+}
+
+.labeled-checkbox input {
+  transform: scale(3);
 }
 </style>
